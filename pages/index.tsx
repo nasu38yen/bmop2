175ebd96350da71150c18d1bd1ab5d80b8ad20f5
島浩文 <hirofumi@peace2.jp>
2022-07-15 18:11:35 +0900

    APIを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,48 @@ import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
+import { useState, useEffect } from 'react';
 
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
+const Home = () => {
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await fetch('/api/projects');
+      const data = await response.json();
+      setMessage(data.text);
+    };
+    fetchMessage();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>What Can I Deploy to Static Apps?</h1>
+      <div className="card-grid">
+        {projects.map((project) => {
+          const Icon = projectIcons[project.id];
+          return (
+            <SmallCard
+              key={project.id}
+              Icon={Icon}
+              title={project.name}
+              slug={project.slug}
+            />
+          );
+        })}
+      </div>
+      <div>{message}</div>
     </div>
-  </div>
-);
+  );
+};
+
+// export const getStaticProps = async () => {
+//   const response = await fetch('/api/projects');
+//   const data = await response.json();
+//   return {
+//     props: {
+//       message: data.text,
+//     },
+//   };
+// };
 
 export default Home;
